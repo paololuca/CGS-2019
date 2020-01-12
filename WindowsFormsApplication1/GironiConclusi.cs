0d68c0c17af4b55f4efb98ebabf266fb03df3b40
paololuca <38073669+paololuca@users.noreply.github.com>
2020-01-12 17:03:47 +0100

    Initial commit

---
+++ WindowsFormsApplication1/GironiConclusi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class GironiConclusi
+    {
+        public Boolean Qualificato { get; set; }
+        public Int32 IdTorneo { get; set; }
+        public Int32 idDisciplina { get; set; }
+        public Int32 IdGirone { get; set; }
+        public Int32 IdAtleta { get; set; }
+        public String NomeAtleta { get; set; }
+        public String CognomeAtleta { get; set; }
+        public Int32 Vittorie { get; set; }
+        public Int32 Sconfitte { get; set; }
+        public Int32 PuntiFatti { get; set; }
+        public Int32 PuntiSubiti { get; set; }
+        public Double Differenziale { get; set; }
+        public Int32 Posizionamento { get; set; }
+        public Double Ranking { get; set; }
+
+    }
+}
